adca3780f1ce4c21d90edbd52789a78d33312ddc
Catus-soup <leoshao59049@gmail.com>
2024-11-19 14:36:29 -0800

    make sapphire tier mine faster but low uses and hi damage

--- UntamedHorizonsBeta-Forge-1.20.1/src/main/java/net/shinysquare/untamedhorizons/item/ModToolTiers.java
+++ UntamedHorizonsBeta-Forge-1.20.1/src/main/java/net/shinysquare/untamedhorizons/item/ModToolTiers.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class ModToolTiers {
     public static final Tier SAPPHIRE = TierSortingRegistry.registerTier(
-            new ForgeTier(5, 1500, 5f, 4f, 25,
+            new ForgeTier(5, 500, 10f, 20f, 25,
                     ModTags.Blocks.NEEDS_SAPPHIRE_TOOL, () -> Ingredient.of(ModItems.SAPPHIRE.get())),
             new ResourceLocation(UntamedHorizons.MOD_ID, "sapphire"), List.of(Tiers.NETHERITE), List.of());
 
